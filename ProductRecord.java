ee347ca695f0164474885134c81f2cff35639da8
dtl1 <61438998+dtl1@users.noreply.github.com>
2020-10-14 19:04:41 +0100

    Update ProductRecord.java

--- ProductRecord.java
+++ ProductRecord.java
@@ -4,40 +4,51 @@ import common.ProductUnavailableException;
 import interfaces.IVendingMachineProduct;
 import interfaces.IProductRecord;
 
+import java.util.ArrayList;
+
 /**
  * This class represents a ProductRecord, recording information relating to a product sold in a vending machine.
- *
  */
 public class ProductRecord implements IProductRecord {
 
+    private final VendingMachineProduct product;
+    private int numSales;
+    private int numAvailable;
+
+    ProductRecord(IVendingMachineProduct product) {
+        this.product = (VendingMachineProduct) product;
+        numSales = 0;
+        numAvailable = 0;
+    }
+
     @Override
     public IVendingMachineProduct getProduct() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.product;
     }
 
     @Override
     public int getNumberOfSales() {
-        // TODO Auto-generated method stub
-        return 0;
+        return numSales;
     }
 
     @Override
     public int getNumberAvailable() {
-        // TODO Auto-generated method stub
-        return 0;
+        return numAvailable;
     }
 
     @Override
     public void addItem() {
-        // TODO Auto-generated method stub
-
+        numAvailable++;
     }
 
     @Override
     public void buyItem() throws ProductUnavailableException {
-        // TODO Auto-generated method stub
-
+        if(numAvailable!= 0) {
+            numAvailable--;
+            numSales++;
+        } else{
+            throw new ProductUnavailableException();
+        }
     }
 
 }
