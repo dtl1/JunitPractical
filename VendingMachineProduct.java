a48ad3da8e5a9730be8442df380a3a78d83fb62d
dtl1 <61438998+dtl1@users.noreply.github.com>
2020-10-14 19:05:03 +0100

    Update VendingMachineProduct.java

--- VendingMachineProduct.java
+++ VendingMachineProduct.java
@@ -4,7 +4,6 @@ import interfaces.IVendingMachineProduct;
 
 /**
  * This class represents products that can be stocked and sold in a vending machine in a specific lane.
- *
  */
 public class VendingMachineProduct implements IVendingMachineProduct {
 
@@ -21,7 +20,7 @@ public class VendingMachineProduct implements IVendingMachineProduct {
         return this.description;
     }
 
-    VendingMachineProduct(String laneCode, String description){
+    VendingMachineProduct(String laneCode, String description) {
         this.laneCode = laneCode;
         this.description = description;
     }
